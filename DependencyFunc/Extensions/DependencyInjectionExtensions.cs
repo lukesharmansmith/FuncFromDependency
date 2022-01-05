6d4b5d9e83c3c2becf2ff028583c1360607bcbe4
Luke Sharman-Smith <lukesharmansmith@live.co.uk>
2022-01-05 21:26:46 +0000

    Add project files.

---
+++ DependencyFunc/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyFunc.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static void AddDiagFunction(this IServiceCollection services)
+        {
+            DiagFunction.Register();
+        }
+    }
+}
---
+++ FuncFromDependency/Startup.cs
@@ -0,0 +1,16 @@
+﻿using DependencyFunc.Extensions;
+
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(FuncFromDependency.Startup))]
+
+namespace FuncFromDependency
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddDiagFunction();
+        }
+    }
+}
